7e509611047105ddd2589af410cf7fbfd72ae3c8
Prathmesh <nirgudeprathmesh1@gmail.com>
2026-05-21 12:13:01 +0530

    create pair of vertices

--- greedy.java
+++ greedy.java
@@ -8,3 +8,17 @@ class Edge{
         weight = w;
     }
 }
+class Pair implements Comparable<Pair>{
+    int vertex;
+    int weight;
+    int source;
+    Pair(int v , int w,int s){
+        vertex = v;
+        weight = w;
+        source  =s;
+    }
+
+    public int compareTo(Pair other){
+        return this.weight - other.weight;
+    }
+}
