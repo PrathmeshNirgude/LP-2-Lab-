d60ef97851c82ca75fc44fe6a710df9d7948b148
Prathmesh <nirgudeprathmesh1@gmail.com>
2026-05-22 12:28:14 +0530

    Update N_Queen_Problem print board fun

--- N_Queen_Problem.java
+++ N_Queen_Problem.java
@@ -1,1 +1,17 @@
 
+import java.util.*;
+
+class N_Qeen_Problem{
+
+    static int n;
+
+    void printBoard(char[][] board){
+        System.out.println("\n\n**Game Board**");
+        for(char[] r: board){
+            for(char c: r){
+                System.out.print(c+" ");
+            }
+            System.out.println();
+        }
+    }
+}
