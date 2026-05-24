5cf33df2f4076536709d4de46b63b2a537afbf17
Prathmesh <nirgudeprathmesh1@gmail.com>
2026-05-24 12:17:01 +0530

    Update Ai_chatbot -  take input and variable declaration

--- Ai_chatbot.java
+++ Ai_chatbot.java
@@ -1,1 +1,13 @@
 
+import java.util.*;
+
+void main(){
+
+    Scanner sc = new Scanner(System.in);
+    StringBuilder s = new StringBuilder();
+    String name = "guest";
+    s.append(" ");
+    String input;
+    String[] process;
+
+}
