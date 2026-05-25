f7f32437db376c0ae3ec052e302dfb8cb24e3687
Prathmesh <nirgudeprathmesh1@gmail.com>
2026-05-25 15:03:49 +0530

    Update Ai_chatbot  -  try to pick name of user

--- Ai_chatbot.java
+++ Ai_chatbot.java
@@ -9,5 +9,34 @@ void main(){
     s.append(" ");
     String input;
     String[] process;
-
+    System.out.println("Official Website");
+    System.out.println("Enter 'bye' to exit.");
+    while (true){
+        s.append("Bot: ");
+        System.out.println("you : ");
+        input = sc.nextLine();
+        input = input.toLowerCase();
+        if(input.contains("my name")||input.contains("i am")){
+            process = input.split(" ");
+            for( int i = 0 ; i < process.length; i++){
+                if(process[i].equals("name")||process[i].equals("am")){
+                    if(process[i+1].equals("is")){
+                        i++;
+                    }
+                    i++;
+                    name = process[i];
+                    break;
+                }
+            }
+            s.append("hi ,"+ name);
+        }
+        else{
+            System.out.println("Bot: sorry i did not understand your question. if issue persist pleas contact support team.");
+            s.setLength(0);
+            s.append("\n");
+        }
+        System.out.println(s);
+        s.setLength(0);
+    }
+    sc.close();
 }
